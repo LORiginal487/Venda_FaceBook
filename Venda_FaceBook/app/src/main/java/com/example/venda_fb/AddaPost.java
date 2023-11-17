528cbfb8788b983639e2258c49c0b5a3ffeb3b50
LORiginal487 <oscarravuku20@gmail.com>
2023-11-17 13:01:04 +0200

    1.0.0.0.0
    .4

---
+++ Venda_FaceBook/app/src/main/java/com/example/venda_fb/AddaPost.java
@@ -0,0 +1,14 @@
+package com.example.venda_fb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddaPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adda_post);
+    }
+}
